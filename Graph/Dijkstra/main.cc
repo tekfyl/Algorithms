aea9e140aecfccf9d382e0783f60d7c2c2298ea3
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-07-06 22:30:39 +0530

    Updated Dijkstra's Algorithm

--- Graph/Dijkstra/main.cc
+++ Graph/Dijkstra/main.cc
@@ -20,23 +20,29 @@ using namespace std;
 
 ll inf=1e18+1;  
 ll N=400; 
-vll v,q,st;
 ll in=0,i=0,j=0,k=0,ans=0;
 ll n=0,m=0,t=0,v1=0,v2=0;
 char c; string s;
-vvi g(N); vi vis(N,0), dis(N,INT_MAX);;
+vvi g(N); vi vis(N,0), dis(N,INT_MAX), v(N);
 map<ii, int> w;
+vi q;
 
 void dijkstra(){
     int s = 1;
-    dis[s] = 0; vis[s] = 1;
-    q.pb(s);
+    dis[s] = 0;
+    priority_queue<ii, vector<ii>, greater<ii> > q;
+    q.push(mk(0,1));
     while(!q.empty()){
-        s = q.front(); q.erase(q.begin()); vis[s] = 1;
-        for(auto v:g[s]){
-            if(!vis[v]){
-                dis[v] = min(dis[v], w[mk(s,v)]+dis[s]);
-                q.pb(v);
+        ii top = q.top();
+        int d = top.fi, s = top.se;
+        q.pop(); vis[s] = 1;
+        for(auto d:g[s]){
+            if(!vis[d]){
+                if(dis[d] > dis[s] + w[mk(s,d)]){
+                    dis[d] = dis[s] + w[mk(s,d)];
+                    v[d] = s;
+                    q.push(mk(dis[d], d));
+                }
             }
         }
     }
@@ -55,7 +61,14 @@ int main(){
     }
     dijkstra();
     cout << endl;
-    for(i=0; i<10; i++) cout << dis[i] << " ";
+    vi ans;
+    int s = n;
+    while(s != 1){
+            ans.pb(s); s = v[s];
+    }
+    ans.pb(1);
+    reverse(all(ans));
+    for(auto d: ans) cout << d << " ";
     //cout<<"\n"<<"Execution time : "<<tick()<<"\n";
     return 0;
 }
